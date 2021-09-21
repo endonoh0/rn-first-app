00e3ec8ad0a259312408fad34682feb905e6a36e
Eric Ho <endonoho@gmail.com>
2021-09-20 20:22:05 -0700

    Add states for goals

--- App.js
+++ App.js
@@ -3,11 +3,27 @@ import React, { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+	const [enteredGoal, setEnteredGoal] = useState('');
+	const [courseGoals, setCourseGoals] = useState([]);
+
+	const goalInputHandler = (enteredText) => {
+		setEnteredGoal(enteredText);
+	}
+
+	const addGoalHandler = () => {
+		setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+	}
+
   return (
-    <View style={styles.container}>
-			<View>
-				<TextInput placeholder="Course Goal" style={styles.input} />
-				<Button title="ADD" />
+    <View style={styles.screen}>
+			<View style={styles.inputContainer}>
+				<TextInput
+					placeholder="Course Goal"
+					style={styles.input}
+					onChangeText={goalInputHandler}
+					value={enteredGoal}
+					/>
+				<Button title="ADD" onPress={addGoalHandler} />
 			</View>
 			<View>
 			</View>
@@ -16,10 +32,17 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
 		padding: 50
   },
+	inputContainer: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center'
+	},
 	input: {
+		width: '80%',
 		borderBottomColor: 'black',
 		borderWidth: 1,
 		padding: 10
