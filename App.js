67c70637983f1355be62b33d866fb6b420842189
Eric Ho <endonoho@gmail.com>
2021-09-20 20:36:51 -0700

    Change scroll view to flat list

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 	const [enteredGoal, setEnteredGoal] = useState('');
@@ -11,7 +11,7 @@ export default function App() {
 	}
 
 	const addGoalHandler = () => {
-		setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+		setCourseGoals(currentGoals => [...currentGoals, { key: Math.random().toString(), value: enteredGoal }]);
 		setEnteredGoal('');
 	}
 
@@ -26,9 +26,15 @@ export default function App() {
 					/>
 				<Button title="ADD" onPress={addGoalHandler} />
 			</View>
-			<ScrollView>
-				{courseGoals.map((goal, i) => <View style={styles.listItem} key={i}><Text>{goal}</Text></View>)}
-			</ScrollView>
+			<FlatList
+				keyExtractor={(item, index) => item.key}
+				data={courseGoals}
+				renderItem={itemData => (
+					<View style={styles.listItem}>
+						<Text>{itemData.item.value}</Text>
+					</View>
+				)}
+			/>
     </View>
   );
 }
