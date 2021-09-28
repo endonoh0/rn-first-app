3e51cb05b672cd907d7315a77cb0fb84f8499521
Eric Ho <endonoho@gmail.com>
2021-09-27 18:27:37 -0700

    Add delete handler

--- App.js
+++ App.js
@@ -7,18 +7,27 @@ import GoalItem from './components/GoalItem';
 export default function App() {
 	const [courseGoals, setCourseGoals] = useState([]);
 
-
 	const addGoalHandler = (goalTitle) => {
-		setCourseGoals(currentGoals => [...currentGoals, { key: Math.random().toString(), value: goalTitle }]);
+		setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: goalTitle }]);
+	};
+
+	const removeGoalHandler = (goalId) => {
+		setCourseGoals(currentGoals => currentGoals.filter(goal => goal.id !== goalId));
 	}
 
   return (
     <View style={styles.screen}>
 			<GoalInput onAddGoal={addGoalHandler} />
 			<FlatList
-				keyExtractor={(item, index) => item.key}
+				keyExtractor={(item, index) => item.id}
 				data={courseGoals}
-				renderItem={itemData => <GoalItem title={itemData.item.value} /> }
+				renderItem={itemData =>
+					<GoalItem
+						id={itemData.item.id}
+						onDelete={removeGoalHandler}
+						title={itemData.item.value}
+					/>
+				}
 			/>
     </View>
   );
